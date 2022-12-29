a80f6186c0cb25a9759f9d502e35d3c4ee90c6bc
harshit136 <83019055+harshit136@users.noreply.github.com>
2022-12-29 20:26:06 +0530

    Till Board

--- src/main/java/com/example/snakeladder/Player.java
+++ src/main/java/com/example/snakeladder/Player.java
@@ -7,6 +7,7 @@ public class Player {
     private Circle coin;
     private String name;
     private int coinPosition;
+    private static Board gameBoard=new Board();
 
     public Player(int tileSize, Color coinColor, String playerName){
         coinPosition=1;
